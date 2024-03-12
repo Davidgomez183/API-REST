0621597eb223ce39d249a6c648c2b070b9accbc3
Davidgomez183 <davidgomezduque03@gmail.com>
2024-03-12 16:34:37 +0100

    buscar un producte per nom que contenga

--- controllers/productSearchController.js
+++ controllers/productSearchController.js
@@ -2,19 +2,21 @@ const Product = require('../models/product');
 
 async function searchProduct(req, res) {
     try {
-        const productName = req.params.productName; // Suponiendo que el nombre del producto está en los parámetros de la solicitud
-        // Obtener el producto de la base de datos por su nombre
-        const product = await Product.findOne({ name: productName });
+        const productName = req.params.productName; 
+        // Crear una expresión regular para buscar nombres que contengan la cadena proporcionada
+        const regex = new RegExp(productName, 'i'); // 'i' indica que la búsqueda sea insensible a mayúsculas y minúsculas
+        // Obtener los productos de la base de datos cuyos nombres coincidan con la expresión regular
+        const products = await Product.find({ name: regex });
        
-        if (!product) {
-            // Si no se encontró ningún producto con el nombre proporcionado, retornar un error 404
-            return res.status(404).json({ error: 'Producto no encontrado' });
+        if (!products || products.length === 0) {
+            // Si no se encontraron productos que coincidan con la búsqueda, retornar un error 404
+            return res.status(404).json({ error: 'Productos no encontrados' });
         }
 
-        // Si se encuentra el producto, responder con el producto encontrado
-        res.status(200).json({ product: product });
+        // Si se encontraron productos, responder con los productos encontrados
+        res.status(200).json({ products: products });
     } catch (error) {
-        console.error('Error al obtener el producto:', error);
+        console.error('Error al obtener los productos:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
     }
 }
